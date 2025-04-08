35d4c6f4fec7c974a8bf9d0be278a3f1d8c30239
svuprof <michael.griffiths@svu.edu>
2025-04-08 09:30:10 -0400

    Create git1python.py

---
+++ git1python.py
@@ -0,0 +1,27 @@
+class Person:
+  def __init__(self, aa, bb, cc):
+    self.name = aa
+    self.age = bb
+    self.id = cc
+    self.street = ""
+    self.city = ""
+    self.zip = ""
+    self.phone = ""
+
+  def myname(self):
+      print("Hello my name is " + self.name)
+
+  def myage(self):
+      print("Hello my age is " + str(self.age))
+
+  def mynameplusage(self):
+      print(self.name +":"+ str(self.age))
+
+class car:
+    def __init__(self, aa, bb):
+        self.make = aa
+        self.model = bb
+
+    def mymake(self):
+        print("Make is", self.make)
+
